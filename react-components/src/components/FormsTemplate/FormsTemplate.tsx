be23ed1c44718f25ddf60b3e434b27ebf871a11d
Grinmak <grinmak@gmail.com>
2023-03-29 17:56:07 +0300

    feat: pass event handlers into child component

--- react-components/src/components/FormsTemplate/FormsTemplate.tsx
+++ react-components/src/components/FormsTemplate/FormsTemplate.tsx
@@ -1,20 +1,28 @@
-import style from './FormsTemplate.module.css';
-import React from 'react';
-// // import FormsName from '../FormsName/FormsName';
-// // import UserCardGenerator from '../UserCardGenerator/UserCardGenerator';
+import React, { ChangeEvent } from 'react';
 
-type IProps = object;
+type IProps = {
+  NameInput: ({ target: { value } }: ChangeEvent<HTMLInputElement>) => void;
+  BirthInput: ({ target: { value } }: ChangeEvent<HTMLInputElement>) => void;
+  LanguageInput: ({ target: { value } }: ChangeEvent<HTMLSelectElement>) => void;
+  ContactInput: ({ target: { value } }: ChangeEvent<HTMLInputElement>) => void;
+  StateData: string;
+};
 type IFormsState = {
   userName: string;
   birthDate: string;
   contact: string;
   selectLanguage: string;
+  rendered: boolean;
 };
 type ILanguage = {
   id: string;
   value: string;
   title: string;
 };
+
+// type IHandlers = {
+//   FormHandler: ({ target: { value } }: ChangeEvent<HTMLInputElement>) => void;
+// };
 type ILanguageArray = Array<ILanguage>;
 
 const languageValues: ILanguageArray = [
@@ -23,184 +31,90 @@ const languageValues: ILanguageArray = [
   { id: 'dw', value: 'dwarvish', title: 'Dwarvish' },
   { id: 'gl', value: 'galach', title: 'Galach' },
 ];
-// const FormsTemplate = (
-//   userNameHandler,
-//   dateOfBirthHandler,
-//   selectLanguageHandler,
-//   submitHandler
-// ) => {
-//   return (
-//     <div className={style.wrapper}>
-//         <div className={style.name}>
 
-//         </div>
-//         {/* <FormsName /> */}
-//         <div>
-//           <label>
-//             Date of birth:
-//             <input
-//               type="date"
-//               name="date"
-//               value={birthDate}
-//               onChange={dateOfBirthHandler}
-//             ></input>
-//           </label>
-//         </div>
-//         <fieldset>
-//           <legend>Contact with me via</legend>
-//           <label>
-//             sms
-//             <input type="checkbox" name="checkbox" onChange={this.contactHandler}></input>
-//           </label>
-//           <label>
-//             e-mail
-//             <input type="checkbox" name="checkbox" onChange={this.contactHandler}></input>
-//           </label>
-//         </fieldset>
-//         <fieldset>
-//           <legend>Gender:</legend>
-//           <div>
-//             <input type="radio" name="gender" value="male" id="female"></input>
-//             <label htmlFor="male">Male</label>
-//           </div>
-//           <input type="radio" name="gender" value="female" id="female"></input>
-//           <label htmlFor="female">Female</label>
-//         </fieldset>
-//         <div>
-//           {' '}
-//           Native Language
-//           <select
-//             value={this.state.selectLanguage}
-//             onChange={this.selectLanguageHandler}
-//             name="test"
-//           >
-//             {languageValues.map(({ id, value, title }) => (
-//               <option key={id} value={value}>
-//                 {title}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-
-//         <label>
-//           Upload Image
-//           <input type="file" name="file"></input>
-//         </label>
-//         <button type="submit">Submit</button>
-//         {/* <textarea value="test"></textarea> */}
-//       </form>
-//     </div>
-//   );
-// };
 class FormsTemplate extends React.Component<IProps, IFormsState> {
-  constructor(props: object) {
+  constructor(props: IProps) {
     super(props);
     this.state = {
-      userName: '',
+      userName: `${this.props.StateData.}`,
       birthDate: '',
       contact: '',
       selectLanguage: '',
+      rendered: false,
     };
+    console.log('props :', this.props);
+    console.log('state :', this.state);
   }
-  userNameHandler = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      userName: value,
-    });
-  };
-  dateOfBirthHandler = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      birthDate: value,
-    });
-    // console.log(this.state.birthDate);
-  };
-
-  contactHandler = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(value);
-    this.setState({
-      contact: value,
-    });
-  };
-  selectLanguageHandler = ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => {
-    // console.log(value);
-    this.setState({
-      selectLanguage: value,
-    });
-  };
-  submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // console.log(value);
-  };
 
   render() {
     return (
-      <div className={style.wrapper}>
-        <form className={style.form} onSubmit={this.submitHandler}>
-          <div>
-            <label>
-              Name:
-              <input
-                name="userName"
-                value={this.state.userName}
-                onChange={this.userNameHandler}
-              ></input>
-            </label>
-          </div>
-          {/* <FormsName /> */}
-          <div>
-            <label>
-              Date of birth:
-              <input
-                type="date"
-                name="date"
-                value={this.state.birthDate}
-                onChange={this.dateOfBirthHandler}
-              ></input>
-            </label>
-          </div>
-          <fieldset>
-            <legend>Contact with me via</legend>
-            <label>
-              sms
-              <input type="checkbox" name="checkbox" onChange={this.contactHandler}></input>
-            </label>
-            <label>
-              e-mail
-              <input type="checkbox" name="checkbox" onChange={this.contactHandler}></input>
-            </label>
-          </fieldset>
-          <fieldset>
-            <legend>Gender:</legend>
+      <>
+        <div>
+          <form onSubmit={this.props.submitHandler}>
             <div>
-              <input type="radio" name="gender" value="male" id="female"></input>
-              <label htmlFor="male">Male</label>
+              <label>
+                Name:
+                <input
+                  name="userName"
+                  value={this.state.userName}
+                  onChange={this.props.NameInput}
+                ></input>
+              </label>
             </div>
-            <input type="radio" name="gender" value="female" id="female"></input>
-            <label htmlFor="female">Female</label>
-          </fieldset>
-          <div>
-            {' '}
-            Native Language
-            <select
-              value={this.state.selectLanguage}
-              onChange={this.selectLanguageHandler}
-              name="test"
-            >
-              {languageValues.map(({ id, value, title }) => (
-                <option key={id} value={value}>
-                  {title}
-                </option>
-              ))}
-            </select>
-          </div>
+            <div>
+              <label>
+                Date of birth:
+                <input
+                  type="date"
+                  name="date"
+                  value={this.state.birthDate}
+                  onChange={this.props.BirthInput}
+                ></input>
+              </label>
+            </div>
+            <fieldset>
+              <legend>Contact with me via</legend>
+              <label>
+                sms
+                <input type="checkbox" name="checkbox" onChange={this.props.ContactInput}></input>
+              </label>
+              <label>
+                e-mail
+                <input type="checkbox" name="checkbox" onChange={this.props.ContactInput}></input>
+              </label>
+            </fieldset>
+            <fieldset>
+              <legend>Gender:</legend>
+              <div>
+                <input type="radio" name="gender" value="male" id="female"></input>
+                <label htmlFor="male">Male</label>
+              </div>
+              <input type="radio" name="gender" value="female" id="female"></input>
+              <label htmlFor="female">Female</label>
+            </fieldset>
+            <div>
+              Native Language
+              <select
+                value={this.state.selectLanguage}
+                onChange={this.props.LanguageInput}
+                name="test"
+              >
+                {languageValues.map(({ id, value, title }) => (
+                  <option key={id} value={value}>
+                    {title}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-          <label>
-            Upload Image
-            <input type="file" name="file"></input>
-          </label>
-          <button type="submit">Submit</button>
-          {/* <textarea value="test"></textarea> */}
-        </form>
-      </div>
+            <label>
+              Upload Image
+              <input type="file" name="file"></input>
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </>
     );
   }
 }
--- react-components/src/pages/FormsPage.tsx
+++ react-components/src/pages/FormsPage.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
+import FormsTemplate from '../components/FormsTemplate/FormsTemplate';
 
-type IProps = object;
-type IFormsState = {
-  userName: string;
-  birthDate: string;
-  contact: string;
-  selectLanguage: string;
-  rendered: boolean;
-};
-type ILanguage = {
-  id: string;
-  value: string;
-  title: string;
-};
-type ILanguageArray = Array<ILanguage>;
-
-const languageValues: ILanguageArray = [
-  { id: 'en', value: 'english', title: 'English' },
-  { id: 'kl', value: 'klingon', title: 'Klingon' },
-  { id: 'dw', value: 'dwarvish', title: 'Dwarvish' },
-  { id: 'gl', value: 'galach', title: 'Galach' },
-];
-
-class FormsTemplate extends React.Component<IProps, IFormsState> {
-  constructor(props: object) {
+class FormsPage extends FormsTemplate {
+  constructor(props) {
     super(props);
     this.state = {
       userName: '',
@@ -34,6 +13,8 @@ class FormsTemplate extends React.Component<IProps, IFormsState> {
     };
   }
   userNameHandler = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('name handler fired');
+
     this.setState({
       userName: value,
     });
@@ -65,78 +46,15 @@ class FormsTemplate extends React.Component<IProps, IFormsState> {
 
   render() {
     return (
-      <>
-        <div>
-          <form onSubmit={this.submitHandler}>
-            <div>
-              <label>
-                Name:
-                <input
-                  name="userName"
-                  value={this.state.userName}
-                  onChange={this.userNameHandler}
-                ></input>
-              </label>
-            </div>
-            {/* <FormsName /> */}
-            <div>
-              <label>
-                Date of birth:
-                <input
-                  type="date"
-                  name="date"
-                  value={this.state.birthDate}
-                  onChange={this.dateOfBirthHandler}
-                ></input>
-              </label>
-            </div>
-            <fieldset>
-              <legend>Contact with me via</legend>
-              <label>
-                sms
-                <input type="checkbox" name="checkbox" onChange={this.contactHandler}></input>
-              </label>
-              <label>
-                e-mail
-                <input type="checkbox" name="checkbox" onChange={this.contactHandler}></input>
-              </label>
-            </fieldset>
-            <fieldset>
-              <legend>Gender:</legend>
-              <div>
-                <input type="radio" name="gender" value="male" id="female"></input>
-                <label htmlFor="male">Male</label>
-              </div>
-              <input type="radio" name="gender" value="female" id="female"></input>
-              <label htmlFor="female">Female</label>
-            </fieldset>
-            <div>
-              Native Language
-              <select
-                value={this.state.selectLanguage}
-                onChange={this.selectLanguageHandler}
-                name="test"
-              >
-                {languageValues.map(({ id, value, title }) => (
-                  <option key={id} value={value}>
-                    {title}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <label>
-              Upload Image
-              <input type="file" name="file"></input>
-            </label>
-            <button type="submit">Submit</button>
-            {/* <textarea value="test"></textarea> */}
-          </form>
-        </div>
-        <div>{this.state.rendered && <h3>test</h3>}</div>
-      </>
+      <FormsTemplate
+        NameInput={this.userNameHandler}
+        BirthInput={this.dateOfBirthHandler}
+        LanguageInput={this.selectLanguageHandler}
+        ContactInput={this.contactHandler}
+        StateData={this.state.userName}
+      />
     );
   }
 }
 
-export default FormsTemplate;
+export default FormsPage;
