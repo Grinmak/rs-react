82ddb2d2bd7aed31bab506d40960fb249c3e21ea
Grinmak <grinmak@gmail.com>
2023-04-11 21:55:19 +0300

    feat: implement warning  message if server return an error

--- react-components/src/pages/HomePage.tsx
+++ react-components/src/pages/HomePage.tsx
@@ -6,24 +6,45 @@ const HomePage = () => {
   const [userSearchValue, setUserSearchValue] = useState('');
   const [dataFromApi, setDataFromApi] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     //timeout  for cross-check
     setTimeout(() => {
       if (userSearchValue === '') {
         fetch('https://rickandmortyapi.com/api/character')
-          .then((res) => res.json())
+          .then((res) => {
+            if (!res.ok) {
+              throw Error('Something went wrong, clear the input and try character name');
+            }
+            return res.json();
+          })
           .then((dataBase) => {
             setDataFromApi(dataBase.results);
             setIsLoading(false);
+            setErrorMessage(null);
+          })
+          .catch((e) => {
+            setIsLoading(false);
+            setErrorMessage(e.message);
           });
       } else {
         const name = userSearchValue.toLowerCase();
         fetch(`https://rickandmortyapi.com/api/character/?name=${name}`)
-          .then((res) => res.json())
+          .then((res) => {
+            if (!res.ok) {
+              throw Error('Wrong input, try character name from Rick and Morty universe');
+            }
+            return res.json();
+          })
           .then((dataBase) => {
             dataBase.results && setDataFromApi(dataBase.results);
             setIsLoading(false);
+            setErrorMessage(null);
+          })
+          .catch((e) => {
+            setIsLoading(false);
+            setErrorMessage(e.message);
           });
       }
     }, 1000);
@@ -46,6 +67,7 @@ const HomePage = () => {
           Data is loading ...
         </div>
       )}
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
       <RickAndMortyCard DataFromApi={dataFromApi} />
     </>
   );
